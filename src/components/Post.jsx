67b0ee14ebcf8d9e89e6c512e45c174963946d61
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-22 18:13:47 -0300

    feat(post-component): disable submit button if textarea is empty and add custom message for required field

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,7 +35,12 @@ export function Post({ author, content, publishedAt }) {
   }
 
   function handleNewCommentChange(event) {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity('Esse campo é obrigatório.');
   }
 
   function deleteComment(commentId) {
@@ -43,6 +48,8 @@ export function Post({ author, content, publishedAt }) {
       prevState.filter((comment) => comment.id !== commentId)
     ));
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -81,10 +88,12 @@ export function Post({ author, content, publishedAt }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Comentar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
